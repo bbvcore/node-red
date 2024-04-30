abc4dc5553fbfc44b821c6735161f4441682f10c
Borja Bas Ventín <37416295+bbvcore@users.noreply.github.com>
2024-04-30 14:20:37 +0200

    Delete TemperaturaInternaESP32conMQTT directory

--- TemperaturaInternaESP32conMQTT/pub.js
+++
@@ -1,9 +0,0 @@
-//*********************************************************************************
-// CLIENTE PARA PUBLICAR MENSAJES MQTT
-//*********************************************************************************
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://localhost:1883');
-client.on('connect', function () {
-    client.publish('/temperature', "{\"temperature\":10}")
-    client.end();
-})
